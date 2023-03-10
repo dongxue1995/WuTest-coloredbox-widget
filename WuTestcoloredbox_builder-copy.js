ed2604e3657bc57296eed6c4807ccb30c6e028f0
Wu Dongxue <91260941+dongxue1995@users.noreply.github.com>
2023-03-10 22:45:11 +0100

    Update WuTestcoloredbox_builder-copy.js

--- WuTestcoloredbox_builder-copy.js
+++ WuTestcoloredbox_builder-copy.js
@@ -11,10 +11,10 @@
 					</tr>
 				</table>
         <table>
-        <tr>
-          <td>Color</td>
-          <td><input id="styling_color" type="text" size="40" maxlength="40"></td>
-        </tr>
+          <tr>
+            <td>Color</td>
+            <td><input id="styling_color" type="text" size="40" maxlength="40"></td>
+          </tr>
         </table>
 				<input type="submit" style="display:none;">
 			</fieldset>
@@ -44,6 +44,7 @@
           detail: {
             properties: {
               opacity: this.opacity,
+              color: this.color,
             },
           },
         })
@@ -57,36 +58,6 @@
     get opacity() {
       return this._shadowRoot.getElementById("builder_opacity").value;
     }
-  }
-
-  customElements.define(
-    "com-sap-sample-coloredbox-builder",
-    ColoredBoxBuilderPanel
-  );
-  class ColoredBoxStylingPanel extends HTMLElement {
-    // template for JavaScript objects.
-    constructor() {
-      super();
-      this._shadowRoot = this.attachShadow({ mode: "open" });
-      this._shadowRoot.appendChild(template.content.cloneNode(true));
-      this._shadowRoot
-        .getElementById("form")
-        .addEventListener("submit", this._submit.bind(this));
-    }
-
-    _submit(e) {
-      e.preventDefault();
-      this.dispatchEvent(
-        new CustomEvent("propertiesChanged", {
-          detail: {
-            properties: {
-              color: this.color,
-            },
-          },
-        })
-      );
-    }
-
     set color(newColor) {
       this._shadowRoot.getElementById("styling_color").value = newColor;
     }
@@ -97,8 +68,7 @@
   }
 
   customElements.define(
-    // defines a custom HTML element
-    "com-sap-sample-coloredbox-styling",
-    ColoredBoxStylingPanel
+    "com-sap-sample-coloredbox-builder",
+    ColoredBoxBuilderPanel
   );
 })();
