c1658450be4a7177ba0de5a59010302217dc514a
Wu Dongxue <91260941+dongxue1995@users.noreply.github.com>
2023-03-14 19:43:36 +0100

    change back to original

--- WuTestcoloredbox_builder-copy.js
+++ WuTestcoloredbox_builder-copy.js
@@ -16,7 +16,6 @@
             <td><input id="styling_color" type="text" size="40" maxlength="40"></td>
           </tr>
         </table>
-        <p>My property value: ${this.myProperty}</p>
 				<input type="submit" style="display:none;">
 			</fieldset>
 		</form>
@@ -30,23 +29,18 @@
 
   class ColoredBoxBuilderPanel extends HTMLElement {
     constructor() {
-      // element created
       super();
       this._shadowRoot = this.attachShadow({ mode: "open" });
       this._shadowRoot.appendChild(template.content.cloneNode(true));
       this._shadowRoot
         .getElementById("form")
         .addEventListener("submit", this._submit.bind(this));
-      this.myProperty = ""; // default value
     }
 
     _submit(e) {
-      // trigger submit when the user submits the form by clicking the submit button
       e.preventDefault();
       this.dispatchEvent(
-        //When the propertiesChanged event is dispatched, it will trigger the onCustomWidgetAfterUpdate method of the ColoredBox custom widget,
         new CustomEvent("propertiesChanged", {
-          //  passing a new instance of CustomEvent as the argument.  propertiesChanged is the event name,
           detail: {
             properties: {
               opacity: this.opacity,
@@ -55,26 +49,6 @@
           },
         })
       );
-    }
-    //---------
-    static get observedAttributes() {
-      return ["my-preperty"];
-    }
-    get myProperty() {
-      return this.myProperty;
-    }
-    set myProperty(value) {
-      this.myProperty = value;
-    }
-    attributeChangedCallback(name, oldValue, newValue) {
-      console.log(oldValue);
-      if (name === "my-property") {
-        this.myProperty = newValue;
-      }
-    }
-    //---------
-    connectedCallback() {
-      this.innerHTML = `<p>My property value: ${this.myProperty}</p>`;
     }
 
     set opacity(newOpacity) {
@@ -95,10 +69,7 @@
   }
 
   customElements.define(
-    //register the element:
     "com-sap-sample-coloredbox-builder",
     ColoredBoxBuilderPanel
   );
-  `;'
-  `;
 })();
